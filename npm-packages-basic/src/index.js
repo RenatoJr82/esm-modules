42b2307e2ef153e30294172cbc49a563df07622e
Renato Roberto <renato.roberto.ajunior@gmail.com>
2026-05-20 22:14:22 -0300

    Aula sobre manipulação do NPM

---
+++ npm-packages-basic/src/index.js
@@ -0,0 +1,5 @@
+import chalk from 'chalk';
+import logSymbols from "log-symbols"
+
+console.log(logSymbols.success, chalk.yellowBright.bgGreen.italic('Olá,!'));
+//console.log(chalk.blackBright.bgCyan.bold("Zéda"))
